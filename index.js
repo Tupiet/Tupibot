42a7400323ddd1709f975cb28fc235f7b0fbac80
Tupiet <Tupiet@users.noreply.github.com>
2020-10-20 19:55:53 +0200

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on("ready", () => {
       type: "PLAYING"
     }
   });
-
+  
 });
 
 // Cada cop que rebem un missatge:
@@ -30,12 +30,10 @@ client.on("message", message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  switch (message.content) {
-    case startsWith("ping"):
-      let ping = Math.floor(message.client.ws.ping);
-      message.channel.send(":ping_pong: `" + ping + " ms.` desde glitch.");
-      break;
-  }
+  if (message.content.startsWith("ping")) {
+    let ping = Math.floor(message.client.ws.ping);
+    message.channel.send(":ping_pong: `" + ping + " ms.` desde glitch.");
+  } 
   
   switch (command) {
     case "aleix":
