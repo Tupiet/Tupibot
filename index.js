43bc7fd3476dea834b312f8b77334f96ab072f1a
Tupiet <Tupiet@users.noreply.github.com>
2020-10-21 18:06:53 +0200

    purge update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on("message", message => {
     case "purge":
       if (message.member.hasPermission("MANAGE_MESSAGES")) {
         if (args[0] == null) {
-          message.channel.send("¡Necesito saber cuántos mensajes quieres eliminar! Por ejemplo, así: `!purge`")
+          message.channel.send("¡Necesito saber cuántos mensajes quieres eliminar! Por ejemplo, así: `!purge 5`")
         } else {
           message.channel.bulkDelete(args[0]);
         }
