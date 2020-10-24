093328a20245e7aabe7252fd4e169dcbb68bdeef
Tupiet <Tupiet@users.noreply.github.com>
2020-10-24 16:10:39 +0200

    Revert "new update"
    This reverts commit fb8f5faf17727805712b5b39ee261a2f8eb7f826.

--- config.js
+++
@@ -1,5 +0,0 @@
-let config = {
-    prefix: "!"
-}
-
-module.exports = config;
--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-let { readdirSync } = require('fs'); 
+let prefix = "!";
 
-client.config = require("./config.js");
+// Determina què farà el bot un cop funcioni sense errors.
+client.on("ready", () => {
 
-client.commandos = new Discord.Collection();
+  console.log("¡Conectado!");
 
-// COMANDOS
+  client.user.setPresence({
+    status: "online",
+    activity: {
+      name: "Jota777 es guapo",
+      type: "PLAYING"
+    }
+  });
+  
+});
 
-for(const file of readdirSync("./comandos")) {
-  if (file.endsWith(".js")) {
-    let fileName = file.substring(0, file.length-3);
-    let fileContents = require("./comandos/${file}");
+// Cada cop que rebem un missatge:
+client.on("message", message => {
 
-    client.commandos.set(fileName, fileContents);
+  // Revisa si l'autor del missatge rebut és un bot.
+  // Si és així, fa un return i ignora tot el següent.
+
+  if (message.author.bot) return;
+  
+  // Agafem el command (el de després del prefix)
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if (message.content.startsWith("ping")) {
+    let ping = Math.floor(message.client.ws.ping);
+    message.channel.send(":ping_pong: `" + ping + " ms.` desde glitch.");
+  } 
+  
+  switch (command) {
+    case "aleix":
+      message.channel.send("¡El mejor!");
+      break;
+    case "ping":
+      message.channel.send("¡Pong!");
+      break;
+    case "purge":
+      if (message.member.hasPermission("MANAGE_MESSAGES")) {
+        if (args[0] == null) {
+          message.channel.send("¡Necesito saber cuántos mensajes quieres eliminar! Por ejemplo, así: `!purge 5`")
+        } else {
+          message.channel.bulkDelete(args[0]);
+        }
+        
+      }
+      
+      break;
+    case "jacob": 
+      message.channel.send("Un gran amigo. ^^");
+      break;
+    case "tupi":
+      message.channel.send("¡La mejor tortuga! :turtle:")
+      break;
   }
-}
+});
 
-// EVENTOS
-
-for (const file of readdirSync("./eventos")) {
-  if (file.endsWith(".js")) {
-    let fileName = file.substring(0, file.length - 3);
-    let fileContents = require(".eventos/${file}");
-
-    client.on(fileName, fileContents.bind(null, client));
-
-    delete require.cache[require.resolve("./eventos/${file}")];
-  }
-}
-
-// LOGIN
-
-client.login(process.env.TOKEN)
-  .then(() => {
-    console.log(`Estoy listo, soy ${client.user.tag}`);
-  })
-  .catch((err) => {
-    console.error("Error al iniciar sesión: " + err);
-  }
-  )
+client.login(process.env.TOKEN);
