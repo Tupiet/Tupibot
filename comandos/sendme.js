8ff77ed8243c316971722031e469e56a9190e494
Tupiet <Tupiet@users.noreply.github.com>
2020-10-30 09:25:40 +0100

    holaa

--- comandos/sendme.js
+++ comandos/sendme.js
@@ -2,6 +2,7 @@ module.exports = (client, message, args) => {
     const user = message.author;
     client.users.fetch(user.id).then((user) => {
         user.send("¡¿Por qué me has llamado?!");
+        user.send("Hola ${user.id}")
     });
   
   }
