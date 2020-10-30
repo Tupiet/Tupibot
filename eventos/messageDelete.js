110fc8884a89022d2ed0fbbd1fafdc706e7ce318
Tupiet <Tupiet@users.noreply.github.com>
2020-10-30 09:31:09 +0100

    ups

--- eventos/messageDelete.js
+++ eventos/messageDelete.js
@@ -1,5 +1,5 @@
 module.exports = (client, message) => {
-    let canal = client.channels.cache.get('ID-CANAL'); 
+    let canal = client.channels.cache.get('768488095208570880'); 
     canal.send(`**${message.author.username}** elimino un mensaje con el contenido: ${message}`);
   
   }
