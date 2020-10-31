d852debb55ec9f916297f20ceec4a152cb1edd41
Tupiet <Tupiet@users.noreply.github.com>
2020-10-31 17:01:53 +0100

    updated the sendme

--- comandos/sendme.js
+++ comandos/sendme.js
@@ -2,7 +2,7 @@ module.exports = (client, message, args) => {
     const user = message.author;
     client.users.fetch(user.id).then((user) => {
         user.send("¡¿Por qué me has llamado?!");
-        user.send("Hola ${user.id}")
+        user.send(`Hola ${persona.username.tag}`)
     });
   
   }
