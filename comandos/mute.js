b34f9c44f08a2481e3f8b03deda9e8a792178e28
Tupiet <Tupiet@users.noreply.github.com>
2020-12-01 17:17:42 +0100

    updated the mute rol

--- comandos/mute.js
+++ comandos/mute.js
@@ -2,8 +2,8 @@ module.exports = (client, message, args) => {
     let persona = message.mentions.users.first();
     if (!persona) persona = message.author;
 
-    persona.roles.add("767023677844750367");
+    persona.roles.add("783366049901838357");
 
-    message.channel.send(`${persona} ha obtenido el rol muerto`)
+    message.channel.send(`${persona} ha obtenido el rol muerto`);
   
   }
