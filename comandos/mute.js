29b78f7a56c06e1672b7b5335967e9288d6a13b5
Tupiet <Tupiet@users.noreply.github.com>
2020-12-01 18:59:18 +0100

    updated the mute rol

--- comandos/mute.js
+++ comandos/mute.js
@@ -1,8 +1,10 @@
 module.exports = (client, message, args) => { 
-    let persona = message.mentions.members.first() || message.member;
+    let persona = message.mentions.members.first() || 
+    message.guild.members.cache.find((m) => m.user.tag === args[0]) || 
+    message.member;
 
     persona.roles.add("783366049901838357");
 
-    message.channel.send(`${persona} ha obtenido el rol muerto`);
+    message.channel.send(`${persona.name} ha obtenido el rol muerto`);
   
   }
