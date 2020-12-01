c6e8bf34771cec206b671b61339336b8de6eb5c4
Tupiet <Tupiet@users.noreply.github.com>
2020-12-01 19:09:41 +0100

    updated the mute rol

--- comandos/unmute.js
+++ comandos/unmute.js
@@ -1,6 +1,7 @@
 module.exports = (client, message, args) => { 
+    let role = message.guild.roles.cache.find((r) => r.name == "Muerto"); // Busca un rol llamado "Muerto". 
     message.guild.members.cache.forEach(member => {
-        member.roles.remove("767023677844750367");
+        member.roles.remove(role.id);
       });
       message.channel.send("Todos los usuarios han sido desmuteados.");
   }
