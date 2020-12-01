fa23d0f0629d83de7f76cda48fc20142d7861c53
Tupiet <Tupiet@users.noreply.github.com>
2020-12-01 20:06:50 +0100

    updated the mute rol

--- comandos/mute.js
+++ comandos/mute.js
@@ -1,4 +1,5 @@
 module.exports = (client, message, args) => { 
+    console.log(message.guild.members.cache.find((m) => m.user.username === args[0]))
     let persona = message.mentions.members.first() || // Usuario a quien se menciona (@Tupi)
     message.guild.members.cache.find((m) => m.user.username === args[0])[0]; // Apodo del usuario (Tupi)
     //message.member; // Usuario que ha enviado el mensaje
