c9bf36bd5bedf63de1424af01fd42b12738ddf1d
Tupiet <Tupiet@users.noreply.github.com>
2020-12-02 16:55:33 +0100

    updated the mute rol

--- comandos/purge.js
+++ comandos/purge.js
@@ -4,10 +4,7 @@ module.exports = (client, message, args) => {
         if (args[0] == null) {
           message.channel.send("¡Necesito saber cuántos mensajes quieres eliminar! Por ejemplo, así: `!purge 5`")
         } else {
-          message.channel
-          .bulkDelete(args[0] + 1)
-          .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
-          .catch(console.error);
+          message.channel.bulkDelete(args[0]);
         }
         
     }
