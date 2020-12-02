e3928af3376ff69bd5cf8bce58170f5fb2a5fdd4
Tupiet <Tupiet@users.noreply.github.com>
2020-12-02 16:45:38 +0100

    updated the mute rol

--- comandos/mute.js
+++ comandos/mute.js
@@ -1,5 +1,5 @@
 module.exports = (client, message, args) => { 
-    let persona = message.mentions.members.first() || message.guild.members.cache.find((m) => m.user.tag === args[0]) || message.member;
+    let persona = message.mentions.members.first() || message.guild.members.cache.filter((m) => m.user.tag === args[0]) || message.member;
 
     let role = message.guild.roles.cache.find(r => r.name == "Muerto");
 
