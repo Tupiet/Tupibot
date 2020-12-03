64f3dc5759bc7f96a5090730ae2cf8cabd672e9a
Tupiet <Tupiet@users.noreply.github.com>
2020-12-03 19:35:16 +0100

    updated the mute rol

--- comandos/mute.js
+++ comandos/mute.js
@@ -1,10 +1,19 @@
 module.exports = (client, message, args) => { 
-    let persona = message.mentions.members.first() || message.member;
+  let role = message.guild.roles.cache.find(r => r.name == "Muerto");
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+      let persona = message.mentions.members.first() || message.member;
 
-    let role = message.guild.roles.cache.find(r => r.name == "Muerto");
+      persona.roles.add(role.id);
 
-    persona.roles.add(role.id);
+      message.channel.send(`${persona} ha obtenido el rol muerto`);
+    }
 
-    message.channel.send(`${persona} ha obtenido el rol muerto`);
+    else {
+      let persona = message.member;
+      persona.roles.add(role.id);
+
+      message.channel.send(`${persona} ha obtenido el rol muerto`);
+    }
+    
   
   }
--- comandos/unmute.js
+++ comandos/unmute.js
@@ -1,8 +1,11 @@
 module.exports = (client, message, args) => { 
-    let role = message.guild.roles.cache.find((r) => r.name == "Muerto"); // Busca un rol llamado "Muerto". 
-    message.guild.members.cache.forEach(member => {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+        let role = message.guild.roles.cache.find((r) => r.name == "Muerto"); // Busca un rol llamado "Muerto". 
+        message.guild.members.cache.forEach(member => {
         member.roles.remove(role.id);
       });
       message.channel.send("Todos los usuarios han sido desmuteados.");
+    }
+    
   }
 
