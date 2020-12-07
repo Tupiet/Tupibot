49c8448e1f1dd834e49953e06b0d9562faa1b90c
Tupiet <Tupiet@users.noreply.github.com>
2020-12-07 13:36:49 +0100

    updated the mute rol

--- comandos/mute.js
+++ comandos/mute.js
@@ -1,16 +1,10 @@
 module.exports = (client, message, args) => { 
-  let role = message.guild.roles.cache.find(r => r.name == "Muerto");
     if (message.member.hasPermission("MANAGE_MESSAGES")) {
       let persona = message.mentions.members.first() || message.member;
-
       persona.voice.setMute(true);
     }
-
     else {
       let persona = message.member;
-      
       persona.voice.setMute(true);
     }
-    
-  
   }
--- comandos/unmute.js
+++ comandos/unmute.js
@@ -1,11 +1,8 @@
 module.exports = (client, message, args) => { 
     if (message.member.hasPermission("MANAGE_MESSAGES")) {
-        
-      message.channel.members.forEach(member => {
+        message.channel.members.forEach(member => {
         member.voice.setMute(false);
       });
-      
     }
-    
   }
 
